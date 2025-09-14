fa2a820061a1ee331f7c281fc8845140d9897bf4
CommandCipher <angads1027@gmail.com>
2025-09-14 14:03:31 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import cv2
+from random import randrange
+
+trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+img = cv2.imread('2people.jpg')
+grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+
+for (x,y,w,h) in face_coordinates:
+    cv2.rectangle(img, (x,y), (x+w, y+h), (randrange(256), randrange(256), randrange(256)), 2)
+
+cv2.imshow('Clever Programmer Face Detector', img)
+cv2.waitKey()
+
+print("Code Completed")
